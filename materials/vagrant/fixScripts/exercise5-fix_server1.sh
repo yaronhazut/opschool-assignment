c3f26485ac4f027c8cb12eadb6b4d60155bf224d
yaronhazut <97437769+yaronhazut@users.noreply.github.com>
2022-09-02 18:11:32 +0300

    Update exercise5-fix_server1.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+su vagrant -c "ssh-keygen -t rsa -N '""' -f /home/vagrant/.ssh/id_rsa"
+cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+cp /home/vagrant/.ssh/id_rsa* /vagrant/.ssh/
+echo -e "Host server2" '\n' '\t' ""StrictHostKeyChecking no"" >> /home/vagrant/.ssh/config
